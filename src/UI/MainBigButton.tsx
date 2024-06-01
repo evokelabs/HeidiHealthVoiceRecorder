d876742b65fafe190b6768f2c52ce16a6648cd66
Adrian Pikios <adrian@evokelabs.com>
2024-06-02 09:23:20 +1000

    refactor: Add offline alert for no transcribe availability

--- src/UI/MainBigButton.tsx
+++ src/UI/MainBigButton.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect } from 'react'
+import { useCallback, useContext, useEffect, useState } from 'react'
 
 import { formatTime } from '@/libs/helpers'
 
@@ -34,6 +34,28 @@ const MainBigButton = ({ iconSVG, caption }: { iconSVG: JSX.Element; caption: st
   // Prevents distracting animation on record button while hovering pause/resume
   const isMouseOverPlayPause = isPressed
 
+  // Check if the user is offline
+  const [isOffline, setIsOffline] = useState(typeof window !== 'undefined' ? !navigator.onLine : true)
+
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      return
+    }
+
+    const goOnline = () => setIsOffline(false)
+    const goOffline = () => setIsOffline(true)
+
+    window.addEventListener('online', goOnline)
+    window.addEventListener('offline', goOffline)
+
+    return () => {
+      window.removeEventListener('online', goOnline)
+      window.removeEventListener('offline', goOffline)
+    }
+  }, [])
+
+  const noTranscribeAvailable = iconSVG.type === TranscribeIconSVG && isOffline
+
   const resetPressed = useCallback(() => {
     if (!setIsPressed) return
     setIsPressed(false)
@@ -52,7 +74,8 @@ const MainBigButton = ({ iconSVG, caption }: { iconSVG: JSX.Element; caption: st
     <>
       <div
         className="flex flex-col gap-4 relative group"
-        onMouseLeave={() => resetPressed()}>
+        onMouseLeave={() => resetPressed()}
+        style={{ pointerEvents: noTranscribeAvailable ? 'none' : 'auto' }}>
         <div
           className={`w-20 h-20 md:w-[7rem] md:h-[7rem] rounded-full bg-white border-4 border-primary drop-shadow transition-all left-0 top-0 relative cursor-pointer flex flex-col justify-center items-center group-hover:drop-shadow-inverse group-hover:top-2 group-hover:left-2.5  ${
             isMouseOverPlayPause ? 'duration-0' : 'duration-150'
@@ -60,7 +83,7 @@ const MainBigButton = ({ iconSVG, caption }: { iconSVG: JSX.Element; caption: st
           role="button">
           {hasTranscribeLength ? <IconTranscribeTimerSVG seconds={seconds} /> : <IconSVG iconSVG={iconSVG} />}
         </div>
-        {/* {iconSVG.type === TranscribeIconSVG && <OfflineAlert />} */}
+        {noTranscribeAvailable && <OfflineAlert />}
       </div>
       <p className="text-center uppercase w-20 md:w-[7rem] font-semibold text-sm md:text-base leading-none md:leading-none select-none mt-4 relative left-1 ">
         {caption}
