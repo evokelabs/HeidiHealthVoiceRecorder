aeff019cdf59b3e463d52c03a39cb4fc54757e05
Adrian Pikios <adrian@evokelabs.com>
2024-06-01 18:19:42 +1000

    Header Layout

---
+++ src/UI/Logos.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import LogosImg from './../../public/img/logos.webp'
+const Logos = () => {
+  return (
+    <div className="flex flex-row items-center gap-1">
+      <Image src={LogosImg} alt="Evoke Labs & Heidi Health Logo" priority width={170} />
+      <h1>
+        Heidi Health
+        <br />
+        Voice Recorder
+      </h1>
+    </div>
+  )
+}
+
+export default Logos
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Logos from '@/UI/Logos'
+
 export default function Home() {
   return (
-    <main className="w-full h-full">
-      
+    <main className="w-full ">
+      <div className="flex justify-center m-6">
+        <Logos />
+      </div>
     </main>
-  );
+  )
 }
