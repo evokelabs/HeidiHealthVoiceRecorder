3028d418a736d460a1e78ef0294f1ff615b832db
Adrian Pikios <adrian@evokelabs.com>
2024-06-02 07:21:05 +1000

    refactor: Add starting layout animation to first two frames

--- src/Layout/LayoutRecordingInit.tsx
+++ src/Layout/LayoutRecordingInit.tsx
@@ -9,11 +9,23 @@ import MainBigButton from '@/UI/MainBigButton'
 import { RecordIconSVG } from '@/UI/SVG/RecordIconSVG'
 
 const LayoutRecordingInit = () => {
-  const { setLayout } = useContext(UIContext)
+  const { setLayout, layoutAnimateIn, layoutAnimateOut, setIsPressed } = useContext(UIContext)
+
+  const animationStyles = {
+    opacity: 1,
+  }
+
+  if (layoutAnimateOut) {
+    animationStyles.opacity = 1
+  }
+
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center gap-5 ">
+    <div
+      className="flex flex-col md:flex-row justify-center items-center gap-5 duration-200 relative"
+      style={animationStyles}>
       <div
         onClick={() => {
+          setIsPressed(true)
           setLayout(LayoutOptions.LayoutRecordingProgressing)
         }}>
         <MainBigButton
--- src/Layout/LayoutRecordingProgressing.tsx
+++ src/Layout/LayoutRecordingProgressing.tsx
@@ -13,16 +13,45 @@ import { RecordIconSVG } from '@/UI/SVG/RecordIconSVG'
 import { StopIconSVG } from '@/UI/SVG/StopIconSVG'
 
 const LayoutRecordingProgressing = () => {
-  const { setIsPressed, setLayout } = useContext(UIContext)
+  const { setIsPressed, setLayout, layoutAnimateIn, layoutAnimateOut } = useContext(UIContext)
   const { isPaused, setIsPaused, startRecording, stopRecording } = useContext(AudioContext)
 
   useEffect(() => {
     startRecording()
   }, [startRecording])
 
+  const animationLeftStyles = {
+    opacity: 1,
+    left: '0px',
+  }
+
+  if (layoutAnimateIn) {
+    animationLeftStyles.opacity = 0
+    animationLeftStyles.left = '50px'
+  }
+
+  if (layoutAnimateOut) {
+    animationLeftStyles.opacity = 0
+  }
+
+  const animationRightStyles = {
+    opacity: 1,
+    left: '0px',
+  }
+
+  if (layoutAnimateIn) {
+    animationRightStyles.opacity = 0
+    animationRightStyles.left = '-50px'
+  }
+
+  if (layoutAnimateOut) {
+    animationRightStyles.opacity = 0
+  }
   return (
     <div className="flex flex-row justify-center items-center gap-7 relative left-14 w-fit m-auto">
       <div
+        className="relative duration-300 ease-out"
+        style={animationLeftStyles}
         onClick={() => {
           stopRecording()
           setLayout(LayoutOptions.LayoutRecordingTranscribe)
@@ -56,9 +85,13 @@ const LayoutRecordingProgressing = () => {
           />
         </div>
       )}
-
-      <RecordingDisplay />
+      <div
+        className="relative duration-300 ease-out"
+        style={animationRightStyles}>
+        <RecordingDisplay />
+      </div>
     </div>
   )
 }
+
 export default LayoutRecordingProgressing
