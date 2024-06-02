64e718cab6333eed591ef209527361fcc897155b
Adrian Pikios <adrian@evokelabs.com>
2024-06-02 12:57:25 +1000

    feature: Add word-by-word animation to transcribe text area

--- src/UI/TranscribeTextArea.tsx
+++ src/UI/TranscribeTextArea.tsx
@@ -3,9 +3,19 @@ import { useContext } from 'react'
 
 const TranscribeTextArea = () => {
   const { transcript } = useContext(SpeechToTextContext)
+  
   return (
     <div className="rounded-2xl p-4 relative bg-white border-primary border-2 mx-10 h-full">
-      <p className="text-left font-semibold text-lg">{transcript}</p>
+      <p className="text-left font-semibold text-lg ">
+        {transcript.split(' ').map((word, index) => (
+          <span
+            key={index}
+            className="word"
+            style={{ animationDelay: `${index * 0.1}s` }}>
+            {word}{' '}
+          </span>
+        ))}
+      </p>
     </div>
   )
 }
